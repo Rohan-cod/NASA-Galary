af492d3f11512033f65a26c3d3c2762f35327071
Rohan Gupta <rohaninjmu@gmail.com>
2023-02-17 12:45:04 +0530

    Initial Commit

---
+++ NASA Galary/NASA_GalaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASA_GalaryApp.swift
+//  NASA Galary
+//
+//  Created by Rohan  Gupta on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct NASA_GalaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
